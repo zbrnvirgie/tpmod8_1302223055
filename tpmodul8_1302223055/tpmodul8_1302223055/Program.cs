ac6cfe0015483c5b741c28f56b4877a74f461c20
zbrnvirgie <163149245+zbrnvirgie@users.noreply.github.com>
2024-04-15 01:43:59 +0700

    Add files via upload

---
+++ tpmodul8_1302223055/tpmodul8_1302223055/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace tpmodul8_1302223055
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CovidConfig config = new CovidConfig();
+            config.LoadConfig("covid_config.json");
+
+            Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + config.SatuanSuhu + ":");
+            double suhuBadan = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+            int hariDeman = int.Parse(Console.ReadLine());
+
+            bool suhuValid = (config.SatuanSuhu == "celcius" && suhuBadan >= 36.5 && suhuBadan <= 37.5) ||
+                             (config.SatuanSuhu == "fahrenheit" && suhuBadan >= 97.7 && suhuBadan <= 99.5);
+            bool hariValid = hariDeman < config.BatasHariDeman;
+
+            if (suhuValid && hariValid)
+            {
+                Console.WriteLine(config.PesanDiterima);
+            }
+            else
+            {
+                Console.WriteLine(config.PesanDitolak);
+            }
+        }
+    }
+}
---
+++ tpmodul8_1302223055/tpmodul8_1302223055/covidConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace tpmodul8_1302223055
+{
+    public class CovidConfig
+    {
+        public string SatuanSuhu { get; set; }
+        public int BatasHariDeman { get; set; }
+        public string PesanDitolak { get; set; }
+        public string PesanDiterima { get; set; }
+
+        public CovidConfig()
+        {
+            SatuanSuhu = "celcius";
+            BatasHariDeman = 14;
+            PesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+            PesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+        }
+
+        public void LoadConfig(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText("covid_config.json");
+                CovidConfig config = JsonConvert.DeserializeObject<CovidConfig>(json);
+                SatuanSuhu = config.SatuanSuhu;
+                BatasHariDeman = config.BatasHariDeman;
+                PesanDitolak = config.PesanDitolak;
+                PesanDiterima = config.PesanDiterima;
+            }
+        }
+
+        public void SaveConfig(string filePath)
+        {
+            string json = JsonConvert.SerializeObject(this);
+            File.WriteAllText(filePath, json);
+        }
+
+        public void UbahSatuan()
+        {
+            SatuanSuhu = SatuanSuhu == "celcius" ? "fahrenheit" : "celcius";
+        }
+    }
+}
